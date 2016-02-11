9abd836d74ff90146e77e6ddb3dc2b834ca0617e
Spencer Gibb <spencer@gibb.us>
2016-02-10 17:25:39 -0700

    initial commit

---
+++ src/test/java/org/springframework/cloud/vault/VaultTests.java
@@ -0,0 +1,27 @@
+package org.springframework.cloud.vault;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = VaultTests.TestApplication.class)
+@WebIntegrationTest()
+public class VaultTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@SpringBootApplication
+	public static class TestApplication {
+
+		public static void main(String[] args) {
+			SpringApplication.run(TestApplication.class, args);
+		}
+	}
+}
