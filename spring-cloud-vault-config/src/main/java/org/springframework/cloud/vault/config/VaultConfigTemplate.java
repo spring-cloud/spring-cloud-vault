19c99b2ca8f39b68e8cc73e5767eee36f4724c0b
Mark Paluch <mpaluch@pivotal.io>
2016-12-15 11:17:00 +0100

    Flatten Vault JSON to Properties.
    Fixes gh-61.

--- spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/VaultConfigTemplate.java
+++ spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/VaultConfigTemplate.java
@@ -15,13 +15,17 @@
  */
 package org.springframework.cloud.vault.config;
 
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.Assert;
 import org.springframework.vault.client.VaultResponseEntity;
 import org.springframework.vault.core.VaultOperations;
-
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.vault.support.JsonMapFlattener;
+import org.springframework.vault.support.VaultResponse;
 
 /**
  * Central class to retrieve configuration from Vault.
@@ -41,8 +45,7 @@ public class VaultConfigTemplate implements VaultConfigOperations {
 	 * @param vaultOperations must not be {@literal null}.
 	 * @param properties must not be {@literal null}.
 	 */
-	public VaultConfigTemplate(VaultOperations vaultOperations,
-			VaultProperties properties) {
+	public VaultConfigTemplate(VaultOperations vaultOperations, VaultProperties properties) {
 
 		Assert.notNull(vaultOperations, "VaultOperations must not be null!");
 		Assert.notNull(properties, "VaultProperties must not be null!");
@@ -56,14 +59,14 @@ public class VaultConfigTemplate implements VaultConfigOperations {
 
 		Assert.notNull(secretBackendMetadata, "SecureBackendAccessor must not be null!");
 
-		VaultResponseEntity<Secrets> response = vaultOperations.doWithVault(
-				new VaultOperations.SessionCallback<VaultResponseEntity<Secrets>>() {
+		VaultResponseEntity<VaultResponse> response = vaultOperations
+				.doWithVault(new VaultOperations.SessionCallback<VaultResponseEntity<VaultResponse>>() {
 					@Override
-					public VaultResponseEntity<Secrets> doWithVault(
+					public VaultResponseEntity<VaultResponse> doWithVault(
 							VaultOperations.VaultSession session) {
 
 						return session.exchange("{backend}/{key}", HttpMethod.GET, null,
-								Secrets.class, secretBackendMetadata.getVariables());
+								VaultResponse.class, secretBackendMetadata.getVariables());
 					}
 				});
 
@@ -71,27 +74,27 @@ public class VaultConfigTemplate implements VaultConfigOperations {
 
 		if (response.getStatusCode() == HttpStatus.OK) {
 
-			Secrets secrets = response.getBody();
-
+			VaultResponse vaultResponse = response.getBody();
+			Map<String, String> data = JsonMapFlattener.flatten(vaultResponse.getData());
 			PropertyTransformer propertyTransformer = secretBackendMetadata
 					.getPropertyTransformer();
 
 			if (propertyTransformer != null) {
-				secrets.setData(
-						propertyTransformer.transformProperties(secrets.getData()));
+				data = propertyTransformer.transformProperties(data);
 			}
 
-			return secrets;
+			return createSecrets(vaultResponse, data);
 		}
 
 		if (response.getStatusCode() == HttpStatus.NOT_FOUND) {
-			log.info(String.format("Could not locate PropertySource: %s",
-					"key not found"));
+			log.info(String
+					.format("Could not locate PropertySource: %s", "key not found"));
 		}
 		else if (properties.isFailFast()) {
-			throw new IllegalStateException(String.format(
-					"Could not locate PropertySource and the fail fast property is set, failing Status %d %s",
-					response.getStatusCode().value(), response.getMessage()));
+			throw new IllegalStateException(
+					String.format(
+							"Could not locate PropertySource and the fail fast property is set, failing Status %d %s",
+							response.getStatusCode().value(), response.getMessage()));
 		}
 		else {
 			log.warn(String.format("Could not locate PropertySource: Status %d %s",
@@ -99,6 +102,24 @@ public class VaultConfigTemplate implements VaultConfigOperations {
 		}
 
 		return null;
+	}
+
+	private Secrets createSecrets(VaultResponse vaultResponse, Map<String, String> data) {
+
+		Secrets secrets = new Secrets();
+
+		secrets.setData(data);
+
+		secrets.setAuth(vaultResponse.getAuth());
+		secrets.setLeaseDuration(vaultResponse.getLeaseDuration());
+		secrets.setMetadata(vaultResponse.getMetadata());
+		secrets.setLeaseId(vaultResponse.getLeaseId());
+		secrets.setRenewable(vaultResponse.isRenewable());
+		secrets.setRequestId(vaultResponse.getRequestId());
+		secrets.setWarnings(vaultResponse.getWarnings());
+		secrets.setWrapInfo(vaultResponse.getWrapInfo());
+
+		return secrets;
 	}
 
 	public VaultOperations getVaultOperations() {
