28868954f348ccdc873b67626f2ad3e23bc10e3a
Mark Paluch <mpaluch@pivotal.io>
2017-05-27 23:26:35 +0200

    Polishing.
    Reformat code.

--- spring-cloud-vault-config/src/test/java/org/springframework/cloud/vault/config/VaultPropertySourceLocatorIntegrationTests.java
+++ spring-cloud-vault-config/src/test/java/org/springframework/cloud/vault/config/VaultPropertySourceLocatorIntegrationTests.java
@@ -1,9 +1,11 @@
 package org.springframework.cloud.vault.config;
 
-import org.junit.Before;
+import java.util.Collections;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,23 +15,20 @@ import org.springframework.cloud.vault.util.VaultRule;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.util.Collections;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Integration test incorporating loading secrets using {@code spring.cloud.vault.applicationName}
- * and active profiles
+ * Integration test incorporating loading secrets using
+ * {@code spring.cloud.vault.applicationName} and active profiles
  *
  * @author Ryan Hoegg
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = VaultPropertySourceLocatorIntegrationTests.TestApplication.class, properties = {
-	"spring.application.name=wintermute",
-	"spring.cloud.vault.application-name=neuromancer",
-	"spring.cloud.vault.generic.application-name=neuromancer,icebreaker"
-})
-@ActiveProfiles({"integrationtest"})
+		"spring.application.name=wintermute",
+		"spring.cloud.vault.application-name=neuromancer",
+		"spring.cloud.vault.generic.application-name=neuromancer,icebreaker" })
+@ActiveProfiles({ "integrationtest" })
 public class VaultPropertySourceLocatorIntegrationTests extends IntegrationTestSupport {
 
 	@BeforeClass
@@ -38,25 +37,21 @@ public class VaultPropertySourceLocatorIntegrationTests extends IntegrationTestS
 		vaultRule.before();
 
 		vaultRule.prepare().getVaultOperations().write("secret/wintermute",
-			Collections.singletonMap("vault.value", "spring.application.name value"));
+				Collections.singletonMap("vault.value", "spring.application.name value"));
 		vaultRule.prepare().getVaultOperations()
-			.write("secret/wintermute/integrationtest",
+				.write("secret/wintermute/integrationtest", Collections.singletonMap(
+						"vault.value", "spring.application.name:integrationtest value"));
+		vaultRule.prepare().getVaultOperations().write("secret/neuromancer", Collections
+				.singletonMap("vault.value", "spring.cloud.vault.applicationName value"));
+		vaultRule.prepare().getVaultOperations().write(
+				"secret/neuromancer/integrationtest",
 				Collections.singletonMap("vault.value",
-					"spring.application.name:integrationtest value"));
-		vaultRule.prepare().getVaultOperations().write("secret/neuromancer",
-			Collections.singletonMap("vault.value",	"spring.cloud.vault.applicationName value"));
+						"spring.cloud.vault.applicationName:integrationtest value"));
+		vaultRule.prepare().getVaultOperations().write("secret/icebreaker",
+				Collections.singletonMap("icebreaker.value", "additional context value"));
 		vaultRule.prepare().getVaultOperations()
-			.write("secret/neuromancer/integrationtest",
-				Collections.singletonMap("vault.value",
-					"spring.cloud.vault.applicationName:integrationtest value"));
-		vaultRule.prepare().getVaultOperations()
-			.write("secret/icebreaker",
-				Collections.singletonMap("icebreaker.value",
-					"additional context value"));
-		vaultRule.prepare().getVaultOperations()
-			.write("secret/icebreaker/integrationtest",
-				Collections.singletonMap("icebreaker.value",
-					"additional context:integrationtest value"));
+				.write("secret/icebreaker/integrationtest", Collections.singletonMap(
+						"icebreaker.value", "additional context:integrationtest value"));
 	}
 
 	@Value("${vault.value}")
@@ -68,21 +63,20 @@ public class VaultPropertySourceLocatorIntegrationTests extends IntegrationTestS
 	@Test
 	public void getsSecretFromVaultUsingVaultApplicationName() {
 		assertThat(configValue)
-			.isEqualTo("spring.cloud.vault.applicationName:integrationtest value");
+				.isEqualTo("spring.cloud.vault.applicationName:integrationtest value");
 	}
 
 	@Test
 	public void getsSecretFromVaultUsingAdditionalContext() {
-		assertThat(additionalValue)
-			.isEqualTo("additional context:integrationtest value");
+		assertThat(additionalValue).isEqualTo("additional context:integrationtest value");
 	}
 
 	@SpringBootApplication
 	public static class TestApplication {
 
 		public static void main(String[] args) {
-			SpringApplication
-				.run(VaultConfigWithContextTests.TestApplication.class, args);
+			SpringApplication.run(VaultConfigWithContextTests.TestApplication.class,
+					args);
 		}
 	}
 
