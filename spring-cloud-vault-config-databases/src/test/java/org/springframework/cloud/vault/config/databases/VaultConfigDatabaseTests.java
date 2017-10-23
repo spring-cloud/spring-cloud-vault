0c19563a828f71810a4b0536ec3b4c73604f9123
Per Abich <per.abich@ig.com>
2017-10-23 07:26:54 +0100

    Found the missing configuration

--- spring-cloud-vault-config-databases/src/test/java/org/springframework/cloud/vault/config/databases/VaultConfigDatabaseTests.java
+++ spring-cloud-vault-config-databases/src/test/java/org/springframework/cloud/vault/config/databases/VaultConfigDatabaseTests.java
@@ -132,7 +132,7 @@ public class VaultConfigDatabaseTests {
     public static class TestApplication {
 
         public static void main(String[] args) {
-            SpringApplication.run(TestApplication.class, args);
+            SpringApplication.run(TestApplication.class, args).registerShutdownHook();
         }
     }
 }
