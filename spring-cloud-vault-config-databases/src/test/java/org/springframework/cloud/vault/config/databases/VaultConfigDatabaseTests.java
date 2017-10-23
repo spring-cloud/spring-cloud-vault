c28231acdafd6161219092261b6f2bc8c85e21d2
Per Abich <per.abich@ig.com>
2017-10-23 10:12:17 +0100

    Added assumtion to let tests pass when testing against older vault versions (<0.7.1)

--- spring-cloud-vault-config-databases/src/test/java/org/springframework/cloud/vault/config/databases/VaultConfigDatabaseTests.java
+++ spring-cloud-vault-config-databases/src/test/java/org/springframework/cloud/vault/config/databases/VaultConfigDatabaseTests.java
@@ -25,6 +25,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.vault.util.CanConnect;
 import org.springframework.cloud.vault.util.VaultRule;
+import org.springframework.cloud.vault.util.Version;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.vault.core.VaultOperations;
 
@@ -80,6 +81,9 @@ public class VaultConfigDatabaseTests {
 
         VaultRule vaultRule = new VaultRule();
         vaultRule.before();
+
+        assumeTrue(vaultRule.prepare().getVersion()
+                .isGreaterThanOrEqualTo(Version.parse("0.7.1")));
 
         if (!vaultRule.prepare().hasSecretBackend(BACKEND)) {
             vaultRule.prepare().mountSecret(BACKEND);
