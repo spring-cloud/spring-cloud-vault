45d627d573f63b8a87175a92bafb0feaa3eeb2ef
Mark Paluch <mpaluch@pivotal.io>
2019-04-18 14:51:55 +0200

    Update Javadoc for internal API.
    Closes gh-327.

--- spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/SecretBackendMetadataFactory.java
+++ spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/SecretBackendMetadataFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2016-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package org.springframework.cloud.vault.config;
 
 /**
  * Strategy interface to create {@link SecretBackendMetadata} from
- * {@link VaultSecretBackendDescriptor} properties.
+ * {@link VaultSecretBackendDescriptor} properties. Mainly for internal use within the framework.
  *
  * <p>
  * Classes implementing this interface must implement
