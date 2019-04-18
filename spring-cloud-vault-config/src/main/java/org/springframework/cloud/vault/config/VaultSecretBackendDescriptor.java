a74253da57a9fc10c6001afceda71c95e66eaded
Mark Paluch <mpaluch@pivotal.io>
2019-04-18 14:51:55 +0200

    Update Javadoc for internal API.
    Closes gh-327.

--- spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/VaultSecretBackendDescriptor.java
+++ spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/VaultSecretBackendDescriptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2018 the original author or authors.
+ * Copyright 2016-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package org.springframework.cloud.vault.config;
 
 /**
  * Interface to be implemented by objects that describe a Vault secret backend.
+ * Mainly for internal use within the framework.
  *
  * <p>
  * Typically used by {@link SecretBackendMetadataFactory} to provide path and
