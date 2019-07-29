c93dc1c958e8e516add29917258d3e94abf74c95
Daniel Franco <dandrfranco@gmail.com>
2019-07-29 09:19:47 +0100

    Update Consul version to 1.5.3.
    See https://github.com/hashicorp/consul/blob/master/CHANGELOG.md#153-july-25-2019
    
    Original pull request: #344.

--- src/test/bash/install_consul.sh
+++ src/test/bash/install_consul.sh
@@ -6,7 +6,7 @@
 ###########################################################################
 
 
-CONSUL_VER="1.5.2"
+CONSUL_VER="1.5.3"
 UNAME=$(uname -s |  tr '[:upper:]' '[:lower:]')
 CONSUL_ZIP="consul_${CONSUL_VER}_${UNAME}_amd64.zip"
 IGNORE_CERTS="${IGNORE_CERTS:-no}"
