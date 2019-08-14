7a5d9972831935c84aa26bd945ced143b809202e
Mark Paluch <mpaluch@pivotal.io>
2019-08-14 10:19:44 +0200

    Add support to override endpointUri for AWS IAM Authentication.
    We now allow setting the AWS IAM Endpoint URI to adjust for various AWS regions.
    
    Closes gh-346.

--- spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/ClientAuthenticationFactory.java
+++ spring-cloud-vault-config/src/main/java/org/springframework/cloud/vault/config/ClientAuthenticationFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2018 the original author or authors.
+ * Copyright 2017-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -293,6 +293,10 @@ class ClientAuthenticationFactory {
 
 		if (StringUtils.hasText(awsIam.getServerName())) {
 			builder.serverName(awsIam.getServerName());
+		}
+
+		if (awsIam.getEndpointUri() != null) {
+			builder.endpointUri(awsIam.getEndpointUri());
 		}
 
 		builder.path(awsIam.getAwsPath()) //
