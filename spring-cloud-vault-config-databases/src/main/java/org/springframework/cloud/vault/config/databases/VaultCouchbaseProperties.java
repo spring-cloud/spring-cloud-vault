9161d58cef29e9118ea712477f8f6875bcd4b4b2
fhitchen@gmail.com <fh562u@att.com>
2020-06-03 14:29:02 -0500

    Add support for Couchbase through vault-plugin-database-couchbase
    Original pull request: gh-408.

---
+++ spring-cloud-vault-config-databases/src/main/java/org/springframework/cloud/vault/config/databases/VaultCouchbaseProperties.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2016-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.vault.config.databases;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * Configuration properties for Vault using the Couchbase integration.
+ *
+ * @author Francis Hitchens
+ */
+@ConfigurationProperties("spring.cloud.vault.couchbase")
+@Validated
+public class VaultCouchbaseProperties implements DatabaseSecretProperties {
+
+	/**
+	 * Enable couchbase backend usage.
+	 */
+	private boolean enabled = false;
+
+	/**
+	 * Role name for credentials.
+	 */
+	private String role;
+
+	/**
+	 * Enable static role usage.
+	 *
+	 */
+	private boolean staticRole = false;
+
+	/**
+	 * Couchbase backend path.
+	 */
+	@NotEmpty
+	private String backend = "database";
+
+	/**
+	 * Target property for the obtained username.
+	 */
+	@NotEmpty
+	private String usernameProperty = "spring.data.couchbase.username";
+
+	/**
+	 * Target property for the obtained password.
+	 */
+	@NotEmpty
+	private String passwordProperty = "spring.data.couchbase.password";
+
+	@Override
+	public boolean isEnabled() {
+		return this.enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	@Override
+	public String getRole() {
+		return this.role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	@Override
+	public boolean isStaticRole() {
+		return this.staticRole;
+	}
+
+	public void setStaticRole(boolean staticRole) {
+		this.staticRole = staticRole;
+	}
+
+	@Override
+	public String getBackend() {
+		return this.backend;
+	}
+
+	public void setBackend(String backend) {
+		this.backend = backend;
+	}
+
+	@Override
+	public String getUsernameProperty() {
+		return this.usernameProperty;
+	}
+
+	public void setUsernameProperty(String usernameProperty) {
+		this.usernameProperty = usernameProperty;
+	}
+
+	@Override
+	public String getPasswordProperty() {
+		return this.passwordProperty;
+	}
+
+	public void setPasswordProperty(String passwordProperty) {
+		this.passwordProperty = passwordProperty;
+	}
+
+}
